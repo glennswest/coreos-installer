7d0047ad275a709410a8805f8de9335a893753bb
gwest <gwest@redhat.com>
2020-03-25 13:23:24 -0500

    Fix test.sh so it can be run multiple times

--- test.sh
+++ test.sh
@@ -1,2 +1,7 @@
+# must yum install bc
+umount /mnt/dl
+rm /tmp/curl-rc
+rm /tmp/image_info
+rm /tmp/image_url
 ./coreos-installer -d sda -i file:/root/coreos-installer/base.ignition -b file:/root/coreos-installer/images/rhcos-4.3.0-x86_64-metal.raw.gz
 
