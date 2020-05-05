c2a40615b2561250659c66a135ded706d6a232e9
gwest <gwest@redhat.com>
2020-05-05 10:55:15 -0500

    Fix test scirpt

--- test.sh
+++ test.sh
@@ -3,5 +3,5 @@ umount /mnt/dl
 rm /tmp/curl-rc
 rm /tmp/image_info
 rm /tmp/image_url
-./coreos-installer -d sda -i file:/root/coreos-installer/base.ignition -b file:/root/coreos-installer/images/rhcos-4.3.0-x86_64-metal.raw.gz
+./coreos-installer -d sda -i file:/root/coreos-installer/tools/base.ignition -b file:/root/coreos-installer/images/rhcos-4.3.0-x86_64-metal.raw.gz
 
