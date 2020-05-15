b91bb32918a3e02301136408e3fe0d3ab25d009c
gwest <gwest@redhat.com>
2020-05-15 10:23:53 -0500

    Working getxdata test

--- getxdata.sh
+++ getxdata.sh
@@ -1,3 +1,14 @@
-export XDATAURL=`cat /proc/cmdline | sed -e 's/^.*xdata=//' -e 's/ .*$//'`
-echo ${XDATAURL}
+export kernelcmdline=`cat /proc/cmdline`
+if [[ $kernelcmdline == *"xdata="* ]]; 
+then
+      export XDATAURL=`cat /proc/cmdline | sed -e 's/^.*xdata=//' -e 's/ .*$//'`
+      echo ${XDATAURL}
+      mkdir -p /var/mnt/datastore
+      mount /dev/sda5 /var/mnt/datastore
+      curl ${XDATAURL} -o /var/mnt/datastore/"${XDATAURL##*/}"
+      umount /var/mnt/datastore
+else
+      echo "No XDATAURL provided"
+fi
+
 
